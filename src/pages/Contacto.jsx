7ad9775ee8fca4c67243162edd1eca0aff89473e
Estuardo Castro <cas23890@uvg.edu.gt>
2025-06-05 17:58:34 -0600

    Vista Contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import FondoConBlur from '../components/FondoconBlur';
-import Kanto from '../assets/Kanto.png'; 
+import InstaIcon from '../assets/Insta.png';
+import Kanto from '../assets/Kanto.png';
+import { useNavigate } from 'react-router-dom';
+import MiCV from '../assets/MiCV.pdf';
+import '../styles/Contacto.css';
 
 const Contacto = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ position: 'relative', minHeight: '100vh', zIndex: 1 }}>
-      <FondoConBlur imagenFondo={Kanto} blur="8px" />
-      <h1 style={{ color: 'white', textAlign: 'center', paddingTop: '50px' }}>
-        Página de Contactos
-      </h1>
+    <div className="contacto-contenedor">
+      <div className="fondo-borroso"></div>
+
+      <button className="boton-home" onClick={() => navigate('./')}>
+        Home
+      </button>
+
+      <div className="contenido-nitido">
+        <h1 className="titulo-contacto">Mis Contactos</h1>
+
+        <div className="instagram-container">
+          <a
+            href="https://www.instagram.com/estuardo_cast04/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="insta-link"
+          >
+            <img src={InstaIcon} alt="Instagram icon" className="insta-img" />
+            <span className="insta-texto">Mi Instagram</span>
+          </a>
+        </div>
+
+        <div className="cv-container">
+          <a href={MiCV} download="Estuardo_Castro_CV.pdf" className="boton-cv">
+            Descargar <span className="flecha">&#8595;</span>
+          </a>
+          <span className="cv-texto">Mi CV</span>
+        </div>
+      </div>
     </div>
   );
 };
